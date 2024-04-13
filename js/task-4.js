6be120c73cbbfb49d9c7d22eecce5f57cd10c9c7
RomaBez <bezvuglyak.roman@gmail.com>
2024-04-14 02:42:46 +0300

    tsk_4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+const onForm = event => {
+    event.preventDefault();
+    
+const formEmail = event.currentTarget.elements.email.value.trim();
+const formPassword = event.currentTarget.elements.password.value.trim();
+if (formEmail === '' || formPassword === '') {
+    return alert('All form fields must be filled in');
+}
+    
+const info = {
+email: formEmail,
+password: formPassword,
+}
+    
+event.currentTarget.reset();
+
+}
+form.addEventListener('submit', onForm);
