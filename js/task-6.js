7dab2894d13c63ab895f38697520493d6c241d06
RomaBez <bezvuglyak.roman@gmail.com>
2024-04-14 05:21:27 +0300

    tsk_6 done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNUmb = document.querySelector('input[type="number"]');
+const crtBtn = document.querySelector('button[data-create]');
+const dstrBtn = document.querySelector('button[data-destroy]');
+const boxesDiv = document.getElementById('boxes');
+
+crtBtn.addEventListener('click', createBoxes);
+dstrBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+    const amount = inputNUmb.value;
+    
+    if (amount < 1 || amount > 100) {
+    }
+    
+    const boxes = [];
+    let boxSize = 30;
+    
+    for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div');
+        box.style.width = `${boxSize}px`;
+        box.style.height = `${boxSize}px`;
+        box.style.backgroundColor = getRandomHexColor();
+        boxes.push(box);
+        boxSize += 10;
+    }
+    
+    boxesDiv.append(...boxes);
+    inputNUmb.form.reset();
+}
+
+function destroyBoxes() {
+    boxesDiv.innerHTML = '';
+}
+
